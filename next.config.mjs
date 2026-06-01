b5cbf80fcb71845e921156dc068210f583e37f74
Chongrim <ck@yissue.biz>
2026-05-31 18:50:21 -0700

    Redirect www domain to apex

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.gamerclock.com',
+          },
+        ],
+        destination: 'https://gamerclock.com/:path*',
+        permanent: true,
+      },
+      {
         source: '/newtitle',
         destination: '/new-releases',
         permanent: true,
